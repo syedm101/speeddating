7dbb97d2c76a5ff527d0be39a309edc8c9f9510f
tonynlin123 <anl5ef@virginia.edu>
2016-10-22 21:45:33 -0400

    Test

--- Lin_Ton_Syed_SpeedDating.R
+++ Lin_Ton_Syed_SpeedDating.R
@@ -7,4 +7,5 @@ summary(date_original)
 #Attributes with more than around 1/3 of the data missing:
 #expnum, attr5_1, sinc5_1, intel5_1, fun5_1, amb5_1, attr1_s, sinc1_s, intel1_s, fun1_s, amb1_s, shar1_s, attr3_s,
 #sinc3_s, intel3_s, fun3_s, amb3_s
+#Test
 which(colMeans(is.na(date_original)) > 1000)
