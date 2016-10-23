0190227e75d83ae92d869154e03ce601181e5112
tonynlin123 <anl5ef@virginia.edu>
2016-10-22 23:16:18 -0400

    *Converted blank strings to NA *Removed attributes w/ more than 3000 missing *Removed irrelevant attributes *Ensured that 1-10 scale data was scaled properly
    **Data log has been updated**

--- Lin_Ton_Syed_SpeedDating.R
+++ Lin_Ton_Syed_SpeedDating.R
@@ -4,8 +4,19 @@ date_original <- read.csv("Speed Dating Data.csv", stringsAsFactors = FALSE)
 View(date_original)
 summary(date_original)
 
-#Attributes with more than around 1/3 of the data missing:
-#expnum, attr5_1, sinc5_1, intel5_1, fun5_1, amb5_1, attr1_s, sinc1_s, intel1_s, fun1_s, amb1_s, shar1_s, attr3_s,
-#sinc3_s, intel3_s, fun3_s, amb3_s
-#Test
-which(colMeans(is.na(date_original)) > 1000)
+#Blank Strings
+date_original[date_original == ""] <- NA
+
+#Attributes with more than around 1/3 of the data missing
+c(names(which(colSums(is.na(date_original))>3000)))
+date <- date_original[,-which(names(date_original) %in% names(which(colSums(is.na(date_original))>3000)))]
+
+#Removed commas in variables
+date[] <- lapply(date, function(x) gsub(",","", x)) #Not sure if this is completely needed
+
+#Removed attributes that did not appear to be useful
+date <- date[,-which(names(date) %in%
+                       c('condtn', 'position', 'positin1', 'undergrd', 'field', 'career'))]
+
+#Viewing ones on a different scale -- appears data from 1 to 10 has been re-scaled
+different_scale <- subset(date, wave %in% c(6,7,8,9))
