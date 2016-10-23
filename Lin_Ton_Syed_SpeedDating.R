a324fd688d6b9fbc75c70324dd761621c477261d
tonynlin123 <anl5ef@virginia.edu>
2016-10-23 17:28:02 -0400

    Fixed character issue with the commas, added correlation metrics and graphs. Ready for imputation

--- Lin_Ton_Syed_SpeedDating.R
+++ Lin_Ton_Syed_SpeedDating.R
@@ -1,3 +1,7 @@
+#Libraries Needed
+library(caret)
+library(corrplot)
+
 #Import Data
 #Data should be sourced within the project, using relative path
 date_original <- read.csv("Speed Dating Data.csv", stringsAsFactors = FALSE)
@@ -12,7 +16,7 @@ c(names(which(colSums(is.na(date_original))>3000)))
 date <- date_original[,-which(names(date_original) %in% names(which(colSums(is.na(date_original))>3000)))]
 
 #Removed commas in variables
-date[] <- lapply(date, function(x) gsub(",","", x)) #Not sure if this is completely needed
+#date[] <- lapply(date, function(x) gsub(",","", x)) #Not sure if this is completely needed, converts all to chars
 
 #Removed attributes that did not appear to be useful
 date <- date[,-which(names(date) %in%
@@ -20,3 +24,39 @@ date <- date[,-which(names(date) %in%
 
 #Viewing ones on a different scale -- appears data from 1 to 10 has been re-scaled
 different_scale <- subset(date, wave %in% c(6,7,8,9))
+
+#Viewing correlations
+correlation_cutoff <- 0.75
+correlationMatrix <- cor(date[,-which(names(date)
+                         %in% c('id', 'iid', 'idg', 'partner', 'pid', 'from','zipcode'))],
+                         use = "complete.obs")
+correlated <- findCorrelation(correlationMatrix, cutoff=correlation_cutoff)
+correlationMatrix2 <- as.data.frame(correlationMatrix)
+print(correlated)
+
+colnames <- colnames(date)
+correlated_names <- colnames[correlated]
+correlationMatrix2 <- correlationMatrix2[,correlated]
+correlationMatrix2 <- subset(correlationMatrix2, (attr1_2 >= correlation_cutoff| attr3_2  >= correlation_cutoff
+                                                  | amb3_2  >= correlation_cutoff | museums  >= correlation_cutoff))
+
+#Graphical Visualization of Correlation
+#Created a subset of the ratng factors
+correlation_subset <- date[,c(15:27,60:89)]
+subset_correlationMatrix <- cor(correlation_subset, use = 'complete.obs')
+
+#Visualization of the correlations within the subset of rating factors
+#Allows for the visualization of 'trade-offs' when marking preferences
+corrplot(subset_correlationMatrix, method = 'circle', type= 'lower', insig = 'blank', main = 'Correlation of Rating Attributes')
+
+#Created another subset for direct comparison (acutal vs. perceived interests, participant vs. partner ratings)
+correlation_subset2 <- date[,c(15:20, 72:77, 21:27, 83:89)]
+subset_correlationMatrix2 <- cor(correlation_subset2, use = 'complete.obs')
+
+#Comparison of correlation with rating attributes
+corrplot(subset_correlationMatrix2, method = 'circle', type= 'lower', insig = 'blank', 
+         main = 'Correlation of Rating Attributes')
+
+#Visualization of the correlations between all attributes (very difficult to see)
+corrplot(correlationMatrix, method = 'circle', type = 'lower', insig = 'blank', 
+         main = 'Comparison of the Correlation of All Attributes')
