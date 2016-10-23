1e3236c9fdae71d4fb9c4dab743634d676f71745
tonynlin123 <anl5ef@virginia.edu>
2016-10-22 21:38:20 -0400

    Created file and uploaded CSV

---
+++ Lin_Ton_Syed_SpeedDating.R
@@ -0,0 +1,10 @@
+#Import Data
+#Data should be sourced within the project, using relative path
+date_original <- read.csv("Speed Dating Data.csv", stringsAsFactors = FALSE)
+View(date_original)
+summary(date_original)
+
+#Attributes with more than around 1/3 of the data missing:
+#expnum, attr5_1, sinc5_1, intel5_1, fun5_1, amb5_1, attr1_s, sinc1_s, intel1_s, fun1_s, amb1_s, shar1_s, attr3_s,
+#sinc3_s, intel3_s, fun3_s, amb3_s
+which(colMeans(is.na(date_original)) > 1000)
