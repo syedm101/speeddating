394b1138ce934bc3be6afa134a41b8c604b56ae8
tonynlin123 <anl5ef@virginia.edu>
2016-10-30 15:32:07 -0400

    Fixed correlation graph, added diff tables

--- Lin_Ton_Syed_SpeedDating.R
+++ Lin_Ton_Syed_SpeedDating.R
@@ -43,41 +43,32 @@ date3 <- date2[-manyNAs(date2),]
 
 #Viewing correlations
 correlation_cutoff <- 0.75
-correlationMatrix <- cor(date[,-which(names(date)
+correlationMatrix <- cor(date3[,-which(names(date3)
                          %in% c('id', 'iid', 'idg', 'partner', 'pid', 'from'))],
                          use = "complete.obs")
 correlated <- findCorrelation(correlationMatrix, cutoff=correlation_cutoff)
-correlationMatrix2 <- as.data.frame(correlationMatrix)
+correlationMatrix2 <- as.data.frame(correlationMatrix[,correlated])
 print(correlated)
 
-correlationMatrix2 <- correlationMatrix2[,correlated]
-correlationMatrix2 <- subset(correlationMatrix2, (attr1_2 >= correlation_cutoff| attr3_2  >= correlation_cutoff
-                                                  | amb3_2  >= correlation_cutoff | museums  >= correlation_cutoff))
+correlationMatrix2 <- subset(correlationMatrix2, (correlationMatrix2 >= correlation_cutoff))
+#Obvious strong correlation between museums and art
 
 #Graphical Visualization of Correlation
 #Created a subset of the ratng factors
-correlation_subset <- date[,c(15:27,60:89)]
+correlation_subset <- date3[,c(18:21,27:56)]
 subset_correlationMatrix <- cor(correlation_subset, use = 'complete.obs')
 
 #Visualization of the correlations within the subset of rating factors
 #Allows for the visualization of 'trade-offs' when marking preferences
-corrplot(subset_correlationMatrix, method = 'circle', type= 'lower', insig = 'blank', main = 'Correlation of Rating Attributes')
-
-#Created another subset for direct comparison (acutal vs. perceived interests, participant vs. partner ratings)
-correlation_subset2 <- date[,c(15:20, 72:77, 21:27, 83:89)]
-subset_correlationMatrix2 <- cor(correlation_subset2, use = 'complete.obs')
-
-#Comparison of correlation with rating attributes
-corrplot(subset_correlationMatrix2, method = 'circle', type= 'lower', insig = 'blank', 
-         main = 'Correlation of Rating Attributes')
+corrplot(subset_correlationMatrix, method = 'circle', type= 'lower', insig = 'blank', 
+         main = 'Correlation of Rating Attributes', mar=c(0,0,1,0))
 
 #Visualization of the correlations between all attributes (very difficult to see)
 corrplot(correlationMatrix, method = 'circle', type = 'lower', insig = 'blank', 
          main = 'Comparison of the Correlation of All Attributes')
 
 #Plot colors
-cbPalette <- c("#ffb3e6", "#56B4E9")
-
+cbPalette <- c("#ffb3e6", "#56B4E9") #Set colors for pink and blue to easily represent males and females in graphs
 
 #### Beginning Data Exploration ####
 female <- subset(date3, date3$gender == 0)
@@ -86,6 +77,7 @@ male <- subset(date3, date3$gender == 1)
 #Males vs. Females Preferences
 means1 <- aggregate(date3,by=list(date3$gender),mean, na.rm = TRUE)
 means1 <- means1[,c('gender', 'attr1_1', 'sinc1_1', 'intel1_1', 'fun1_1', 'amb1_1', 'shar1_1')]
+means1_diff <- means1[1,] - means1[2,] #Difference between 0 and 1
 means1 <- rename(means1, c('attr1_1' = 'Attractiveness', 'sinc1_1' = 'Sincerity', 'intel1_1' = 'Intelligence', 
                  'fun1_1' = 'Fun', 'amb1_1' = 'Ambition', 'shar1_1' = 'Shared Interests'))
 means1 <-melt(means1,id.vars="gender")
@@ -107,7 +99,8 @@ means2f <- subset(means2f, gender == 0)
 means2f <- rename(means2f, c('attr1_1' = 'Attractiveness', 'sinc1_1' = 'Sincerity', 'intel1_1' = 'Intelligence', 
                              'fun1_1' = 'Fun', 'amb1_1' = 'Ambition', 'shar1_1' = 'Shared Interests'))
 
-means2c <- rbind(means2m, means2f)
+means2c <- rbind(means2f, means2m)
+means2c_diff <- means2c[1,] - means2c[2,]
 means2c <-melt(means2c,id.vars="gender")
 
 means2_graph <- ggplot(means2c,aes(x=variable,y=value,fill=factor(gender)))+ geom_bar(stat="identity",position="dodge")+
@@ -127,7 +120,8 @@ means3m <- subset(means3m, gender == 1)
 means3m <- rename(means3m, c('attr1_1' = 'Attractiveness', 'sinc1_1' = 'Sincerity', 'intel1_1' = 'Intelligence', 
                              'fun1_1' = 'Fun', 'amb1_1' = 'Ambition', 'shar1_1' = 'Shared Interests'))
 
-means3c <- rbind(means3m, means3f)
+means3c <- rbind(means3f, means3m)
+means3c_diff <- means3c[1,] - means3c[2,]
 means3c <-melt(means3c,id.vars="gender")
 
 cbPalette <- c("#56B4E9", "#ffb3e6")
